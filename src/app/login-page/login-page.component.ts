5ba1f301203161e6aaf13c211a57f2978cee40b1
CAG2127 <tecnicocesrb23@gmail.com>
2021-11-06 14:30:47 -0500

    firts commit

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UserService } from '../user.service';
 
 @Component({
@@ -6,12 +6,11 @@ import { UserService } from '../user.service';
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent{
 userService: UserService
   constructor(userService: UserService) { this.userService = userService}
 
-  ngOnInit(): void {
-  }
+
 
   userName = ""
   
